60c8680cf3f56512e2a734691594629227ee81db
rothr4 <rothr4.rrrr@gmail.com>
2019-02-04 02:19:55 +0900

    小テスト作成までの処理をパイプライン化したシェルを追加

---
+++ duo/question-tool/bin/question.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# DUO小テスト作成用のpythonスクリプトをパイプライン化
+
+
+# alias question='bash ~/hanaya-san/bellflower/duo/question-tool/bin/question.sh' 等でエイリアス追加しておいて下さい
+
+
+if [ $# -ne 3 ]; then
+  echo "Wrong number of arguments." 1>&2
+  exit 1
+fi
+
+echo "ST_IDX: $1"
+echo "ED_IDX: $2"
+echo "ITEMS_CNT: $3"
+
+cd `dirname $0`
+
+# 外部サイトにアクセスしてデータをDLする。
+# (今後設定ファイル管理でココはON/OFFする)
+python ../python/getDuoFile.py
+
+# 問題文・解答ファイル作成スクリプト実行
+python ../python/createExamFileOfEng.py $1 $2 $3
+
+echo "Fin."
